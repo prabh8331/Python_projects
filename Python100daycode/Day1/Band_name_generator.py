32bc3ede27cab491f952fffd0bc71084afdcae4e
prabh8331 <prabh8331@gmail.com>
2023-09-10 02:15:09 +0530

    Day 1 - python coding

---
+++ Python100daycode/Day1/Band_name_generator.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("Welcome to Band name generator.\n")
+#2. Ask the user for the city that they grew up in.
+city = input("In which city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+band_name = city + " " + pet
+#5. Make sure the input cursor shows on a new line:
+print("Your band name could be: " + band_name)
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
---
+++ Python100daycode/Day1/main.py
@@ -0,0 +1,50 @@
+#Write your code below this line 👇
+
+#print("""Day 1 - Python Print Function
+#The function is declared like this:
+#print('what to print')""")
+
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+
+#Fix the code below 👇
+#print(Day 1 - String Manipulation")
+#print("String Concatenation is done with the "+" sign.")
+#  print('e.g. print("Hello " + "world")')
+#print(("New lines can be created with a backslash and n.")
+
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+#input function
+print(len(input("What is your name? ")))
+
+
+#Write a program that switches the values stored in the variables a and b.
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a 
+
+a = b 
+
+b = c 
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+

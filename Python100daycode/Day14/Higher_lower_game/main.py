90adef7caea09bb65c415bf67e96eafc9d24a4a0
Prabh Singh <prabh8331@gmail.com>
2023-10-07 03:05:38 +0530

    day14

---
+++ Python100daycode/Day14/Higher_lower_game/main.py
@@ -0,0 +1,36 @@
+# Welcome msg import logo & vs from art.py
+from art import logo, vs
+from game_data import data
+import random as r
+
+print(logo)
+
+# a variable = True which become False when player give wrong input 
+continue_game = True
+
+
+# make a random variable selection function which will 
+# also ensure new name is not repeated but if all the names in variable are covered this function
+# then reset and again do the same
+
+def rand_sel():
+    print(len(data))
+
+
+
+# Create a while loop which will run until game is over
+
+while continue_game == True:
+
+    ans = input("What is your answer? 'A' or 'B':\n")
+    
+    if 
+
+
+
+    
+    # b/w word 1 and word 2 there should be vs 
+
+    # there should be two variables word 1 and word 2, after all the ans word 1 should = word 2   and word 2 be random 
+
+    # if condition - if player ans is wrong then this condition will get true and variable = False 

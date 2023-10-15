1615d3c46e7fdc36053929182bd068b59e33b419
Prabh Singh <prabh8331@gmail.com>
2023-10-16 01:23:40 +0530

    coffee_maker_opp

--- Python100daycode/Day16/oop-coffee-machine-start/main.py
+++ Python100daycode/Day16/oop-coffee-machine-start/main.py
@@ -2,24 +2,24 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-ask_menu = Menu()
+menu = Menu()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
-turn_on = True
+is_on = True
 
-while turn_on:
-    choice = input(f"What would you like? ({ask_menu.get_items()}): ").lower()
+while is_on:
+    choice = input(f"What would you like? ({menu.get_items()}): ").lower()
 
     if choice == "report":
         coffee_maker.report()
         money_machine.report()
 
     elif choice == "off":
-        turn_on = False
+        is_on = False
 
     else:
-        drink = ask_menu.find_drink(choice)
+        drink = menu.find_drink(choice)
         if drink != None and coffee_maker.is_resource_sufficient(drink):
             if money_machine.make_payment(drink.cost):
                 coffee_maker.make_coffee(drink)

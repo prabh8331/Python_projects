89220b07ca1462d918b2c79107fd9091a5c502b2
Prabh Singh <prabh8331@gmail.com>
2023-10-19 22:45:45 +0530

    quiz game oop

--- Python100daycode/Day17/Quiz Project/data.py
+++ Python100daycode/Day17/Quiz Project/data.py
@@ -1,6 +1,6 @@
 """This is a question bank"""
 
-question_data = [
+question_data_old = [
     {"text": "A slug's blood is green.", "answer": "True"},
     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
@@ -14,3 +14,117 @@ question_data = [
     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
 ]
+
+####  trivia database -->  https://opentdb.com/    --> API ---> update the API parameters (select type = True/false)--> 
+#### Generate the api url -->copy anp and paste on new tab --> copy that data and paste here . 
+
+
+question_data = {
+    "response_code": 0,
+    "results": [
+        {
+            "category": "Entertainment: Video Games",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Peter Molyneux was the founder of Bullfrog Productions.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Video streaming website YouTube was purchased in it&#039;s entirety by Facebook for US$1.65 billion in stock.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "Science & Nature",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "Like with the Neanderthals, Homo sapiens sapiens also interbred with the Denisovans.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "The sum of all the numbers on a roulette wheel is 666.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "The United States Department of Homeland Security was formed in response to the September 11th attacks.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Adolf Hitler was a german soldier in World War I.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "Entertainment: Board Games",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "&quot;PAYDAY: The Heist&quot; is a sequel to the board game &quot;Payday&quot;.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "boolean",
+            "difficulty": "hard",
+            "question": "The USS Missouri (BB-63) last served in the Korean War.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "Finnish Lapphund dogs were used for herding reindeer.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "The pickled gherkin was first added to hamburgers because a US health law required all fast-food to include a source of Vitamin C.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        }
+    ]
+}
+
+
+print(question_data["results"][0]["question"])
+print(question_data["results"][0]["correct_answer"])
--- Python100daycode/Day17/Quiz Project/main.py
+++ Python100daycode/Day17/Quiz Project/main.py
@@ -2,6 +2,7 @@
 
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -10,8 +11,24 @@ question_bank = []
 
 #here we are converting string key "text" into an object 
 
+question_data = question_data["results"]  ## addition to make new format of data work
+
+
 for qn in question_data:
-    txt = qn["text"]
-    ans = qn["answer"]
+    # txt = qn["text"]
+    # ans = qn["answer"]
+    txt = qn["question"]   #addition to make new data work
+    ans = qn["correct_answer"]   #addition to make new data work
     new_question = Question(txt,ans)
     question_bank.append(new_question)
+
+
+quiz=QuizBrain(question_bank)
+
+quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")

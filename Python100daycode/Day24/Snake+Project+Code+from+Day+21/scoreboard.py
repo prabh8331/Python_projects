477d8f1a55fc127c29ea0bf5ba27d6afa2b5c91d
Prabh Singh <prabh8331@gmail.com>
2023-11-09 01:46:49 +0530

    file system managment python

---
+++ Python100daycode/Day24/Snake+Project+Code+from+Day+21/scoreboard.py
@@ -0,0 +1,43 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        # self.high_score = 0
+        #read the file content
+        with open("Python100daycode/Day24/Snake+Project+Code+from+Day+21/data.txt") as data:
+            self.high_score = int(data.read())
+
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}, High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    # new method to reset instead of doing game over
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            #write into the file
+            with open("Python100daycode/Day24/Snake+Project+Code+from+Day+21/data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()

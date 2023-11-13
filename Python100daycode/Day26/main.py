cb883b8a08dce7212f8ffc1881b526155b7cfc16
Prabh Singh <prabh8331@gmail.com>
2023-11-13 12:56:38 +0530

    Comprehensions

---
+++ Python100daycode/Day26/main.py
@@ -0,0 +1,9 @@
+# ## Dictionary Comprehension
+
+# Syntax: 
+# new_dict = {new_key: new_value for item in list} <br>
+# new_dict = {new_key:new_value for (key,value) in dict.items()}
+# new_dict = {new_key:new_value for (key,value) in dict.items() if test}
+
+## NATO : Phonetic alphabet, codes & signals
+

33c6f6428df3dfcaf52698ebe12a20d263f7d456
prabh8331 <prabh8331@gmail.com>
2023-12-03 16:35:33 +0000

    API - ISS detect

---
+++ Python100daycode/Day33/run.sh
@@ -0,0 +1,5 @@
+python3 Python100daycode/Day33/main.py
+
+duration=$(<"Python100daycode/Day33/signal")
+
+echo "bash /home/coder/project/Python_projects/Python100daycode/Day33/run.sh" | at now + 1 $duration

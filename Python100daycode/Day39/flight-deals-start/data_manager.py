86c6bb24bff0d65080d0fe94dcae92bc148534d6
Prabh Singh <prabh8331@gmail.com>
2023-12-19 21:40:03 +0530

    flight price project

--- Python100daycode/Day39/flight-deals-start/data_manager.py
+++ Python100daycode/Day39/flight-deals-start/data_manager.py
@@ -1,24 +1,31 @@
 import requests
-import os
-
-user_name = os.environ.get("mypassappuser")
-user_code = os.environ.get("mypassappcode")
-timeout = 10
-pass_app_url = os.environ.get("mypassappurl")
-get_data_url = pass_app_url + "get_data"
-response = requests.get(get_data_url, auth=(user_name, user_code), timeout=timeout)
-pass_data = response.json()
-
-api_sheety = pass_data["prabh8331@gmail.com"]["sheety_token"]
-
-headers_sheety = {
-"Authorization": f"Bearer {api_sheety}"
-}
-
-sheety_url = "https://api.sheety.co/69ff39b78b52b370199fe9f25bb1ec09/flightDeals/prices"
+import pass_app 
 
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    response = requests.get(url=sheety_url,headers=headers_sheety,timeout=timeout)
-    excel_data = response.json()
+    def __init__(self):
+        
+        self.timeout=10
+        self.my_email = "prabh8331@gmail.com"
+        self.api_sheety = pass_app.PassApp().get_data(user=self.my_email,app="sheety_token")
+        
+        self.headers_sheety = {
+        "Authorization": f"Bearer {self.api_sheety}"
+        }
+        
+        self.sheety_url = "https://api.sheety.co/69ff39b78b52b370199fe9f25bb1ec09/flightDeals/prices"
+
+    def get_data(self):
+        # response = requests.get(url=self.sheety_url,headers=self.headers_sheety,timeout=self.timeout)
+        data  = {'prices': [{'city': 'Paris', 'iataCode': '', 'lowestPrice': 54, 'id': 2}, 
+                            {'city': 'Berlin', 'iataCode': '', 'lowestPrice': 42, 'id': 3}, 
+                            {'city': 'Tokyo', 'iataCode': '', 'lowestPrice': 485, 'id': 4}, 
+                            {'city': 'Sydney', 'iataCode': '', 'lowestPrice': 551, 'id': 5}, 
+                            {'city': 'Istanbul', 'iataCode': '', 'lowestPrice': 95, 'id': 6}, 
+                            {'city': 'Kuala Lumpur', 'iataCode': '', 'lowestPrice': 414, 'id': 7}, 
+                            {'city': 'New York', 'iataCode': '', 'lowestPrice': 240, 'id': 8}, 
+                            {'city': 'San Francisco', 'iataCode': '', 'lowestPrice': 260, 'id': 9}, 
+                            {'city': 'Cape Town', 'iataCode': '', 'lowestPrice': 378, 'id': 10}]}
+        return data
+        # return response.json()
     

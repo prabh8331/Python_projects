06e3998e34052d7f6c42d5ae7ceac8601d18ee88
Prabh Singh <prabh8331@gmail.com>
2023-12-20 22:39:03 +0530

    flight club

--- Python100daycode/Day39/flight-deals-start/flight_data.py
+++ Python100daycode/Day39/flight-deals-start/flight_data.py
@@ -1,3 +1,8 @@
 class FlightData:
     #This class is responsible for structuring the flight data.
-    pass
+    def __init__(self,data):
+        self.local_departure = data["data"][0]["local_departure"]
+        self.local_arrival = data["data"][0]["local_arrival"]
+        self.price = data["data"][0]["price"]
+        self.duration = data["data"][0]["duration"]
+
